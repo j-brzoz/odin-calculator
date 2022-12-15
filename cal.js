61c6b750be6614015d23b7dc2654f08aac3197e4
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2022-12-15 11:10:12 +0100

    added basic functions

---
+++ cal.js
@@ -0,0 +1,31 @@
+function add(a, b){
+    return a + b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function substract(a, b){
+    return a - b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(operator, a, b){
+    if(operator == "+")
+        return add(a, b);
+    else if(operator == "-")
+        return substract(a, b);
+    else if(operator == "*")
+        return multiply(a, b);
+    else if(operator == "/")
+        return divide(a, b);
+}
+
+console.log(operate("+", 5, 3));
+console.log(operate("-", 5, 3));
+console.log(operate("*", 5, 3));
+console.log(operate("/", 5, 3));
